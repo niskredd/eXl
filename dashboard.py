e05cf3b59feb3a02b58d0af5ba41b2f9c29f40d5
Nils <niskredd@nmbu.no>
2022-01-09 08:28:14 +0100

    update dash

--- dashboard.py
+++ dashboard.py
@@ -2,13 +2,46 @@ import pandas as pd
 import plotly.express as px
 import sreamlit as st
 
+st.set_page_config(page_title='Prosjekt info',
+                   page_icon=":bar_chart:",
+                   layout="wide"
+)
+
+
+
 df = pd.read_excel(
     io='book.xlsx',
     engine='openpyxl',
-    sheet_name='Fyll ut først',
+    sheet_name='Fylles ut først',
     usecols='A:D',
     nrows=20,
 )
 
 
-print(df)
+st.dataframe(df)
+
+
+# ---- Sidebar ----
+st.sidebar.header("Plese Filter Here:")
+company = st.sidebar.multiselect(
+    "Select Company:",
+    option=df[''].unique(),
+    defalut=df[''].unique()
+)
+
+active = st.sidebar.multiselect(
+    "Select Status:",
+    option=df[''].unique(),
+    defalut=df[''].unique()
+)
+
+df_selection = df.query(
+    "Company == @company & Status == @active"
+)
+
+
+st.title(":bar_chart: Prosjekt Dashbord")
+st.markdown("##")
+
+
+totale = int(df_selection['Fakturert'].sum())
