a6b50e090bbd8663ed12eee32d9f3e60c0ebacdb
Nils <niskredd@nmbu.no>
2022-01-16 12:46:01 +0100

    rewriting

--- dsah_main.py
+++ dsah_main.py
@@ -2,7 +2,9 @@
 import dashboard
 import new_prodject
 import streamlit as st
-PAGES = {
+from streamlit_pages.streamlit_pages import MultiPage
+
+"""PAGES = {
     "Prosjektliste" : dashboard,
     "Legg til nytt prosjekt" : new_prodject
 }
@@ -10,4 +12,21 @@ st.sidebar.title('Valg')
 selection = st.sidebar.radio("Gå til ...", list(PAGES.keys()))
 pages = PAGES[selection]
 
+"""
 
+def dashdef():
+    if st.button("Dashbord"):
+        st.write(dashboard)
+
+
+def newdef():
+    if st.button("Nytt prosjekt"):
+        st.write(new_prodject)
+
+
+# call app class object
+app = MultiPage()
+# Add pages
+app.add_page("Dashbord", dashdef)
+app.add_page("Nytt prosjekt", newdef)
+app.run()
